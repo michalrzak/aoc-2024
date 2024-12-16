949bb9d0d1594563d33a6714b9c1d0e8f8dc2428
michalrzak <michal.r.zak@protonmail.com>
2024-12-16 09:34:27 +0100

    day 16

---
+++ day_16_2.jl
@@ -0,0 +1,62 @@
+function main()
+  raw = read("inputs/day16.txt", String)
+  lines = split(raw, "\n")
+  pop!(lines)  # remove last empty line
+
+  field = hcat([[c for c in line] for line in lines]...)
+
+  start_pos = findfirst(==('S'), field)
+  end_pos = findfirst(==('E'), field)
+
+  directions = map(x -> CartesianIndex(x), [(1, 0), (-1, 0), (0, 1), (0, -1)])
+  current_dir = CartesianIndex(1, 0)
+  current_pos = start_pos
+
+  node_costs = Dict()
+  visited1 = Set()
+  visited2 = Set()
+  visited3 = Set()
+  current_cost = 0
+  path = Set([current_pos])
+  while current_pos != end_pos
+    if current_pos in visited2
+      push!(visited3, current_pos)
+    elseif current_pos in visited1
+      push!(visited2, current_pos)
+    else
+      push!(visited1, current_pos)
+    end
+    for dir in directions
+      new_pos = current_pos + dir
+      if field[new_pos] == '.' || field[new_pos] == 'E'
+        cost = current_cost + 1
+        if dir != current_dir
+          cost += 1000
+        end
+
+        if ~haskey(node_costs, new_pos) || node_costs[new_pos][1] > cost
+          path_copy = copy(path)
+          push!(path_copy, new_pos)
+          node_costs[new_pos] = (cost, dir, path_copy)
+        elseif node_costs[new_pos][1] == cost
+          path_copy = copy(path)
+          push!(path_copy, new_pos)
+          path_copy = union(path_copy, node_costs[new_pos][3])
+          node_costs[new_pos] = (cost, dir, path_copy)
+        end
+
+      end
+    end
+    unvisited = filter(x -> ~(x[1] in visited3), node_costs)
+    (_, current_pos) = findmin(x -> x[1], unvisited)
+    current_cost = node_costs[current_pos][1]
+    current_dir = node_costs[current_pos][2]
+    path = node_costs[current_pos][3]
+    delete!(node_costs, current_pos)
+  end
+  println(length(path))
+
+end
+
+main()
+
